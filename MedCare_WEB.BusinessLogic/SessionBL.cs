1b628f64776ca7bc9b8686e52da4a58e1cb367d9
Caraman Daniela <caramandaniela001@gmail.com>
2024-04-12 01:01:49 +0300

    Initialization login

--- MedCare_WEB.BusinessLogic/SessionBL.cs
+++ MedCare_WEB.BusinessLogic/SessionBL.cs
@@ -1,17 +1,30 @@
 ﻿using MedCare_WEB.BusinessLogic.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MedCare_WEB.Domains.Entities.User;
+using project_CAN.BusinessLogic.Core;
+using System.Web;
 
-namespace MedCare_WEB.BusinessLogic
+namespace project_CAN.BusinessLogic
 {
-    public class SessionBl : ISession
+    public class SessionBL : UserApi, ISession
     {
-        //public bool UserLogin(ULoginData uLoginData)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        public UResponseLogin UserLoginSessionBL(ULoginData data)
+        {
+            return UserLoginAction(data);
+        }
+
+        public HttpCookie GenCookie(string loginCredential)
+        {
+            return Cookie(loginCredential);
+        }
+
+        public UserMinimal GetUserByCookie(string apiCookieValue)
+        {
+            return UserCookie(apiCookieValue);
+        }
+
+        public UResponseLogin UserRegistrationSessionBL(URegistrationData data)
+        {
+            return UserRegistrationAction(data);
+        }
     }
 }
---
+++ MedCare_WEB.Domains/Entities/User/UDbTable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MedCare_WEB.Domains.Entities.Enums;
+
+namespace project_CAN.Domain.Entities.User
+{
+    [Table("users")]
+    public class UDBTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int userId { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Username cannot be longer than 50 characters.")]
+        public string firstName { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Username cannot be longer than 50 characters.")]
+        public string lastName { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Username cannot be longer than 50 characters.")]
+        public string phoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password cannot be shorter than 8 characters.")]
+        public string password { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        [StringLength(256)]
+        public string email { get; set; }
+
+        [Required]
+        [Column(TypeName = "int")]
+        public URole privilegies { get; set; }
+
+        public int? appointmentID { get; set; }
+
+        [Required]
+        public bool blocked { get; set; }
+
+        [Required]
+        public DateTime lastLogin { get; set; }
+        
+    }
+}
